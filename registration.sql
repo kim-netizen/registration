570dcd465880faf86a34aea802025d48ebfde02f
EGavilan-Media <45221488+EGavilan-Media@users.noreply.github.com>
2019-06-02 02:30:14 -0400

    Update registration.sql

--- registration.sql
+++ registration.sql
@@ -2,15 +2,15 @@ CREATE DATABASE registration;
 
 USE registration;
 
-CREATE TABLE user(
-				id INT(11) NOT NULL AUTO_INCREMENT,  
-				firstname VARCHAR(50) NOT NULL,
+CREATE TABLE users(
+			id INT(11) NOT NULL AUTO_INCREMENT,  
+			firstname VARCHAR(50) NOT NULL,
         		lastname VARCHAR(50) NOT NULL,
-				email VARCHAR(50) NOT NULL,
-				username VARCHAR(50) NOT NULL,
-				password text(50) NOT NULL,  
+			email VARCHAR(50) NOT NULL,
+			username VARCHAR(50) NOT NULL,
+			password text(50) NOT NULL,  
         		gender VARCHAR(10) NOT NULL,  
-				birthday DATE NOT NULL,
-				created_date DATETIME NOT NULL,
-				PRIMARY KEY(id)
-					);
+			birthday DATE NOT NULL,
+			created_date DATETIME NOT NULL,
+			PRIMARY KEY(id)
+				);
